3b959df96e2fc2ec2a6f694c9493a7eb5960c01c
Weslley Santos <weslleyqi0@gmail.com>
2022-05-22 19:56:13 -0300

    Initial commit

---
+++ app/src/main/java/com/weslleyqi0/guiamg/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.weslleyqi0.guiamg.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
